98840a50d90b8044dbf7618d0c75c7cc0c50a7af
kitkatyourmom <sarahlridings@gmail.com>
2024-04-08 22:01:37 -0600

    add responsiveness for smaller screens

--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 import styles from "./Navbar.module.css";
+import {getImageUrl} from "../../utils";
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return <nav className={styles.navbar}>
         <a className={styles.title} href="/">Portfolio</a>
         <div className={styles.menu}>
+            <img 
+                className={styles.menuBtn} 
+                src={
+                    menuOpen
+                        ? getImageUrl("nav/menuIcon.png")
+                        : getImageUrl("nav/menuIcon.png")
+                } 
+                alt="menu-button"
+                onClick={() => setMenuOpen(!menuOpen)}
+            />
             <ul className={styles.menuItems}>
                 <li>
                     <a href="#about">About</a>
