257a7462152f6706fdfcbf773ac4a7a22fdf5e36
kitkatyourmom <sarahlridings@gmail.com>
2024-04-08 22:13:34 -0600

    show / hide for small menu on the navbar

--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -19,7 +19,9 @@ export const Navbar = () => {
                 alt="menu-button"
                 onClick={() => setMenuOpen(!menuOpen)}
             />
-            <ul className={styles.menuItems}>
+            <ul 
+            className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+            onClick={() => setMenuOpen(false)}>
                 <li>
                     <a href="#about">About</a>
                 </li>
