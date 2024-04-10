c3497e69212f12594fe1e2da83ccb148351c2332
kitkatyourmom <sarahlridings@gmail.com>
2024-04-09 18:56:57 -0600

    add styling for the about section in About.module.css

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -1,20 +1,38 @@
 import React from "react";
+
+import styles from "./About.module.css";
 import { getImageUrl } from "../../utils";
 
 export const About = () => {
     return (
-        <section>
-            <div>
+        <section className={styles.container}>
+            <h2 className={styles.title}>About</h2>
+            <div className={styles.content}>
                 <img
                 src={getImageUrl("about/aboutImage.png")}
                 alt="Me sitting with a laptop"
+                className={styles.aboutImage}
                 />
-                <ul>
-                    <li>
+                <ul className={styles.aboutItems}>
+                    <li className={styles.aboutItem}>
                         <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
-                        <div>
+                        <div className={styles.aboutItemText}>
                             <h3>Frontend Developer</h3>
-                            <p>I'm a frontend developer with experience in building responsive and optomized sites</p>
+                            <p>I'm a frontend developer with experience in building responsive and optimized sites</p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>Backend Developer</h3>
+                            <p>I have experience developing fast and optimized back-end systems.</p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/cursorIcon.png")} alt="UI Icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>UI designer</h3>
+                            <p>I have designed things</p>
                         </div>
                     </li>
                 </ul>
