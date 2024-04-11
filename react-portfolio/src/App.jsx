c03950e1c9354b46c7d6b480fa99258178a2c29d
kitkatyourmom <sarahlridings@gmail.com>
2024-04-11 12:55:35 -0600

    add Experience component

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,6 +1,7 @@
 
 import styles from "./App.module.css";
 import { About } from "./components/About/About";
+import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 
@@ -13,6 +14,7 @@ function App() {
         <Navbar />
         <Hero />
         <About />
+        <Experience />
       </div>
     </>
   )
---
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import styles from "./Experience.module.css";
+import { getImageUrl } from '../../utils';
+
+export const Experience = () => {
+    return <div>
+        Experience
+    </div>;
+};
