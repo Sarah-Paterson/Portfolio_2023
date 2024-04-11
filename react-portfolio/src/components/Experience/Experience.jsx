2d9a04782e7e8372d44de3eda5b17b80cf98a645
kitkatyourmom <sarahlridings@gmail.com>
2024-04-11 13:07:54 -0600

    import information from skills.json to Experience.jsx

--- react-portfolio/src/components/Experience/Experience.jsx
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
+import skills from "../../data/skills.json";
 import styles from "./Experience.module.css";
 import { getImageUrl } from '../../utils';
 
 export const Experience = () => {
-    return <div>
-        Experience
-    </div>;
+    return <section id='experience'>
+        <h2>Experience</h2>
+        <div>
+            <div>{
+                skills.map((skill, id) => {
+                    return <div key={id}>
+                        <div>
+                            <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                        </div>
+                    </div>
+                })
+                }</div>
+            <ul>
+            </ul>
+        </div>
+    </section>;
 };
