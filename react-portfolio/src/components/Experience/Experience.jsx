295c687bc0e58384029f9ea4ab28f90787b1a5f9
kitkatyourmom <sarahlridings@gmail.com>
2024-04-11 13:22:11 -0600

    incorporated data from history.json to Experience.jsx

--- react-portfolio/src/components/Experience/Experience.jsx
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import skills from "../../data/skills.json";
+import history from "../../data/history.json";
 import styles from "./Experience.module.css";
 import { getImageUrl } from '../../utils';
 
@@ -14,10 +15,26 @@ export const Experience = () => {
                         <div>
                             <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
                         </div>
+                        <p>{skill.title}</p>
                     </div>
                 })
                 }</div>
             <ul>
+                {
+                    history.map((historyItem, id) => {
+                        return <li key={id}>
+                            <img src={getImageUrl(historyItem.imageSrc)} 
+                            alt={`${historyItem.organisation} Logo`} />
+                            <div>
+                                <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                                <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                                <ul>{historyItem.experiences.map((experience, id) => {
+                                    return <li key={id}>{experience}</li>
+                                })}</ul>
+                            </div>
+                        </li>
+                    })
+                }
             </ul>
         </div>
     </section>;
