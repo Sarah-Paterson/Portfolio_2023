f5ed144de4f9ee0255fcd1e56fa54564111dfc53
kitkatyourmom <sarahlridings@gmail.com>
2024-04-12 15:13:02 -0600

    add react icons

--- react-portfolio/src/components/Contact/Contact.jsx
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -4,7 +4,7 @@ import styles from "./Contact.module.css";
 import { getImageUrl } from '../../utils';
 
 export const Contact = () => {
-    return <footer className={styles.container}>
+    return <footer className={styles.container} id='contact'>
         <div className={styles.text}>
             <h2>Contact</h2>
             <p>Feel free to reach out!</p>
