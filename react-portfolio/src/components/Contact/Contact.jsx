95d2d1ed13e39851b687fbfc1aa8f4cf19149705
kitkatyourmom <sarahlridings@gmail.com>
2024-04-11 18:26:08 -0600

    add styling to footer

--- react-portfolio/src/components/Contact/Contact.jsx
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -5,20 +5,20 @@ import { getImageUrl } from '../../utils';
 
 export const Contact = () => {
     return <footer className={styles.container}>
-        <div>
+        <div className={styles.text}>
             <h2>Contact</h2>
             <p>Feel free to reach out!</p>
         </div>
-        <ul>
-            <li>
+        <ul className={styles.links}>
+            <li className={styles.link}>
                 <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
                 <a href="mailto:sarahlridings@gmail.com">sarahlridings@gmail.com</a>
             </li>
-            <li>
+            <li className={styles.link}>
                 <img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIn icon" />
                 <a href="https://www.linkedin.com/in/sarahlpaterson">linkedin.com/in/sarahlpaterson</a>
             </li>
-            <li>
+            <li className={styles.link}>
                 <img src={getImageUrl("contact/githubIcon.png")} alt="GitHub icon" />
                 <a href="https://github.com/Sarah-Paterson">github.com/Sarah-Paterson</a>
             </li>
