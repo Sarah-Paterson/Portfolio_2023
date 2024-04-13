343ad4a58edbdf8f15f84b6ef48f4853bdb8e418
kitkatyourmom <sarahlridings@gmail.com>
2024-04-13 10:54:47 -0600

    minor adjustments to contact and projects

--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -15,11 +15,11 @@ export const Projects = () => {
     return <section className={styles.container} id='projects'>
         <h2 className={styles.title}>Projects</h2>
         <Tabs
-            defaultActiveKey="profile"
+            defaultActiveKey="programming"
             id="project-tabs"
             className="mb-3"
             >
-            <Tab tabClassName={styles.tab} eventKey="home" title="Programming">
+            <Tab tabClassName={styles.tab} eventKey="programming" title="Programming">
                 <div className={styles.projects}>
                     {codeprojects.map((project, id) => {
                         return (
@@ -28,7 +28,7 @@ export const Projects = () => {
                     })}
                 </div>
             </Tab>
-            <Tab tabClassName={styles.tab} eventKey="profile" title="Graphic Design">
+            <Tab tabClassName={styles.tab} eventKey="graphic-design" title="Graphic Design">
                 <div className={styles.projects}>
                     {designprojects.map((project, id) => {
                         return (
@@ -37,7 +37,7 @@ export const Projects = () => {
                     })}
                 </div>
             </Tab>
-            <Tab tabClassName={styles.tab} eventKey="contact" title="Fine Art">
+            <Tab tabClassName={styles.tab} eventKey="fine-art" title="Fine Art">
                 <div className={styles.projects}>
                     {artprojects.map((project, id) => {
                         return (
