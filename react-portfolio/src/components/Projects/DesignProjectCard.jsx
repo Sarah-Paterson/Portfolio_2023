d9b0ff0ad2e78c290bb74a3f91317a79c869c26c
kitkatyourmom <sarahlridings@gmail.com>
2024-04-12 22:51:55 -0600

    add tabs to projects

--- react-portfolio/src/components/Projects/DesignProjectCard.jsx
+++ react-portfolio/src/components/Projects/DesignProjectCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import styles from "./ProjectCard.module.css";
 import { getImageUrl } from '../../utils';
 
-export const CodeProjectCard = ({ project : { title, imageSrc, description, skills, demo, source } }) => {
+export const DesignProjectCard = ({ project : { title, imageSrc, description, skills, demo, source } }) => {
     return <div className={styles.container}>
         <img src={getImageUrl(imageSrc)} alt={`image of ${title}`} className={styles.image} />
         <h3 className={styles.title}>{title}</h3>
--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 
-import projects from "../../data/code-projects.json";
+import codeprojects from "../../data/code-projects.json";
+import artprojects from "../../data/art-projects.json";
+import designprojects from "../../data/design-projects.json";
 import styles from "./Projects.module.css";
 import { CodeProjectCard } from './CodeProjectCard';
+import { ArtProjectCard } from './ArtProjectCard';
+import { DesignProjectCard } from './DesignProjectCard';
 
 export const Projects = () => {
     return <section className={styles.container} id='projects'>
@@ -17,7 +21,7 @@ export const Projects = () => {
             >
             <Tab tabClassName={styles.tab} eventKey="home" title="Programming">
                 <div className={styles.projects}>
-                    {projects.map((project, id) => {
+                    {codeprojects.map((project, id) => {
                         return (
                             <CodeProjectCard key={id} project={project}/>
                         );
@@ -25,10 +29,22 @@ export const Projects = () => {
                 </div>
             </Tab>
             <Tab tabClassName={styles.tab} eventKey="profile" title="Graphic Design">
-                Tab content for Profile
+                <div className={styles.projects}>
+                    {designprojects.map((project, id) => {
+                        return (
+                            <DesignProjectCard key={id} project={project}/>
+                        );
+                    })}
+                </div>
             </Tab>
             <Tab tabClassName={styles.tab} eventKey="contact" title="Fine Art">
-                Tab content for Contact
+                <div className={styles.projects}>
+                    {artprojects.map((project, id) => {
+                        return (
+                            <ArtProjectCard key={id} project={project}/>
+                        );
+                    })}
+                </div>
             </Tab>
         </Tabs>
     </section>;
