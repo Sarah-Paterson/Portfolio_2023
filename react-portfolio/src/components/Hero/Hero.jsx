11c02fb83a3a52c8c6927bebf6fe231ac9023f17
kitkatyourmom <sarahlridings@gmail.com>
2024-04-13 11:15:29 -0600

    add resume download to hero

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -11,7 +11,10 @@ export const Hero = () => {
                 I have a bachelor's degree in art and design and a certificate in full-stack web development, which allow me to combine my creativity and technical skills. 
                 Reach out if you'd like to learn more!
             </p>
-            <a href='mailto:sarahlridings@gmail.com' className={styles.contactBtn}>Contact Me</a>
+            <div className={styles.btnContainer}>
+                <a href='mailto:sarahlridings@gmail.com' className={styles.btn}>Email Me!</a>
+                <a href="../../assets/hero/Sarah_Paterson-UIUX_Designer-Graphic_Designer.pdf" download="Sarah_Paterson_CV" className={styles.btn}>Download My CV</a>
+            </div>
         </div>
         <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg} />
             <div className={styles.topBlur} />
