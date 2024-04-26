d3837dd64c51924a3c5751733f336ea7e43a0507
kitkatyourmom <sarahlridings@gmail.com>
2024-04-26 00:15:25 -0600

    update about section information

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -19,9 +19,15 @@ export const About = () => {
                         <div className={styles.aboutItemText}>
                             <h3>Web Developer</h3>
                             <h4>Front-End Web Development</h4>
-                            <p>I'm a frontend developer with experience in building responsive and optimized sites.</p>
+                            <p>I’m a front-end developer with experience in building responsive and optimized sites. This website was created with React; however, I have experienced and am comfortable with a variety of other front-end programs working individually or as part of a team.</p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/cursor.png")} alt="Cursor Icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>Web Developer</h3>
                             <h4>Back-End Web Development</h4>
-                            <p>I have experience developing fast and optimized back-end systems.</p>
+                            <p>I have experience developing fast and optimized back-end systems. I have experienced a variety of back-end programs and have designed back-end for my own projects as well as assisted with a team.</p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
@@ -29,19 +35,23 @@ export const About = () => {
                         <div className={styles.aboutItemText}>
                             <h3>Graphic Designer</h3>
                             <h4>Art Direction</h4>
-                            <p>I have experience developing fast and optimized back-end systems.</p>
+                            <p>I have over eight years of professional experience as a graphic designer and over two years as an art director where I managed a team to ensure design consistency throughout.</p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/magic-wand.png")} alt="Design Icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>Graphic Designer</h3>
                             <h4>Promotional Branding</h4>
-                            <p>I have experience developing fast and optimized back-end systems.</p>
+                            <p>I have worked directly with clients to prepare and modify current designs or help create completely new designs for promotional products.</p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
                         <img src={getImageUrl("about/paintbrush-pencil.png")} alt="Art Icon" />
                         <div className={styles.aboutItemText}>
                             <h3>Fine Artist</h3>
-                            <h4>Painting</h4>
-                            <p>I have experience developing fast and optimized back-end systems.</p>
-                            <h4>Printing</h4>
-                            <p>I have experience developing fast and optimized back-end systems.</p>
+                            <p>I have a bachelor’s degree in fine art and over ten years of experience as an artist. My artwork has been displayed in a local shop, a local art studio, and one piece was displayed in the Denver Art Museum.</p>
+
                         </div>
                     </li>
                 </ul>
